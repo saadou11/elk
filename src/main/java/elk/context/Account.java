cc054bc6b164eb57eb62d7eb9390d3bd6cecc4ea
Saad MAHI <saadallah.mahi@gmail.com>
2017-06-10 02:24:43 +0100

    adding account class

---
+++ src/main/java/elk/context/Account.java
@@ -0,0 +1,12 @@
+package elk.context;
+
+public class Account implements Comparable<Object>{
+	
+	private String accountNumber;
+
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
